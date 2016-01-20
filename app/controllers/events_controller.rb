a53b1056515f2a2f0f33bf73584f6963d6185044
shad0wfox <l45632100@hotmail.com>
2016-01-21 04:25:08 +0800

    before_action test

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,8 @@
+
 class EventsController < ApplicationController
+	
+	before_action :set_event, :only => [ :show, :edit, :update, :destroy]
+
 	def index
 		@events = Event.all
 	end
@@ -15,23 +19,17 @@ class EventsController < ApplicationController
 	end
 
 	def show
-		@event = Event.find(params[:id])
 		@page_title = @event.name
 	end
 
 	def edit
-		@event = Event.find(params[:id])
 	end
 
 	def update
-		@event = Event.find(params[:id])
-		@event.update(event_params)
-
 		redirect_to :action => :show, :id => @event
 	end
 
 	def destroy
-		@event =  Event.find(params[:id])
 		@event.destroy
 
 		redirect_to :action =>  :index
@@ -41,5 +39,9 @@ class EventsController < ApplicationController
 
 	def event_params
 		params.require(:event).permit(:name, :description)
+	end
+
+	def set_event
+		@event = Event.find(params[:id])
 	end
 end
