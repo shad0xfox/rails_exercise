3fe5cf31caf34bf13ba4890292ab242bc512b817
shad0wfox <l45632100@hotmail.com>
2016-01-25 06:17:07 +0800

    design a event has_many :attendees

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :events
+  resources :events do
+    resources :attendees, :controller => 'event_attendees'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
