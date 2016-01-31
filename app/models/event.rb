82fc466151db0529be74a0bd797bad063c05ad57
shad0wfox <l45632100@hotmail.com>
2016-02-01 02:17:13 +0800

    Nested Model

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,5 @@ class Event < ActiveRecord::Base
 	has_many :groups, :through => :event_groupships
 	has_one :location
 	belongs_to :category
+	accepts_nested_attributes_for :location, :allow_destroy => true, :reject_if => :all_blank
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :events do
     resources :attendees, :controller => 'event_attendees'
+    resource :location, :controller => 'event_locations'
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
